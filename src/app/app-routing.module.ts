1c597772644c32a10a8793de82e23e580e6a6b09
DUC_THANH\phith <phithanh365@gmai.com>
2024-03-13 18:08:20 +0700

    g

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'',redirectTo:'client/Home',pathMatch:'full'},
-  {path:'/client/Home',redirectTo:'client/Home',pathMatch:'prefix'},
+  {path:'client/Home',redirectTo:'client/Home',pathMatch:'prefix'},
 ];
 
 @NgModule({
