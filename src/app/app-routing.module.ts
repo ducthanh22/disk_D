38ca7ee2fc50e6d08d41614ea2573bab7d763a5b
DUC_THANH\phith <phithanh365@gmai.com>
2024-03-13 17:50:36 +0700

    h

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientClienttemplateComponent } from './Layout/template/client-clienttemplate/client-clienttemplate.component';
 
 const routes: Routes = [
   // {path:'',redirectTo:'Login',pathMatch:'full'},
 
-  {
+ {
     path: '',
     data: {
-      title: 'Default',
+      title: 'Trang',
     },
     children: [
       {
         path: '',
-        loadChildren: () =>
-          import('./Layout/template/template.module').then(
-            (x) => x.TemplateModule
-          ),
-      },
-    ],
-  },
+        component: ClientClienttemplateComponent,
+        data: {
+          title: 'Client',
+        },
+        children: [
+          {
+            path: 'client',
+            loadChildren: () =>
+              import('../app/modules/client/client.module').then(
+                (x) => x.ClientModule
+              ),
+          },
+          
+        ],
+      }
+    ]
+  }
   
 ];
 
--- src/app/modules/client/client-routing.module.ts
+++ src/app/modules/client/client-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
   
   
       {
-        path: '',
+        path: 'Home',
         component:HomeComponent,
         title: 'Home',
       },
